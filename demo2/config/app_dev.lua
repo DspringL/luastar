492445ddc9ed4d49e23d9cbf32f18d52a261460e
liwenzhen <D5209421@163.com>
2024-06-19 11:07:06 +0800

    feat：修复aes错误

--- demo2/config/app_dev.lua
+++ demo2/config/app_dev.lua
@@ -2,18 +2,22 @@
 应用配置文件
 --]]
 mysql = {
-    host = "127.0.0.1",
+    host = "172.17.20.224",
     port = "3306",
-    user = "root",
-    password = "root123",
-    database = "luastar-cms",
+    user = "luastar_user",
+    password = "5Nnjg0kPcziZGfXD",
+    database = "luastar_db",
     timeout = 30000,
     pool_size = 1000
 }
 redis = {
-    host = "127.0.0.1",
+    host = "172.17.20.224",
     port = "6379",
-    auth = "xxx",
+    auth = {
+        username = "opentresty_user",
+        password = "G0HHCczDhRQSZlnk"
+    },
+    db_index = 9,
     timeout = 30000,
     pool_size = 1000
 }
--- luastar/libs/resty/session/ciphers/aes.lua
+++ luastar/libs/resty/session/ciphers/aes.lua
@@ -24,7 +24,7 @@ local CIPHER_SIZES = {
 local defaults = {
     size   = CIPHER_SIZES[var.session_aes_size] or 256,
     mode   = CIPHER_MODES[var.session_aes_mode] or "cbc",
-    hash   = hashes[var.session_aes_hash]       or "sha512",
+    hash   = hashes[var.session_aes_hash]       or "sha256",
     rounds = tonumber(var.session_aes_rounds)   or 1
 }
 
@@ -33,21 +33,47 @@ local cipher = {}
 cipher.__index = cipher
 
 function cipher.new(config)
-    local a = config.aes or defaults
+    local a = config and config.aes or defaults
     return setmetatable({
         size   = CIPHER_SIZES[a.size or defaults.size]   or 256,
         mode   = CIPHER_MODES[a.mode or defaults.mode]   or "cbc",
-        hash   = hashes[a.hash       or defaults.hash]   or hashes.sha512,
+        hash   = hashes[a.hash       or defaults.hash]   or hashes.sha256,
         rounds = tonumber(a.rounds   or defaults.rounds) or 1
     }, cipher)
 end
 
-function cipher:encrypt(d, k, s)
-    return aes:new(k, s, cip(self.size, self.mode), self.hash, self.rounds):encrypt(d)
+-- 处理salt长度，salt要么为nil要么长度为8
+local function salt_substring(str)
+    if str == nil or string.len(str) < 8 then
+        return nil
+    end
+
+    local str_len = string.len(str)
+    if str_len == 8 then
+        return str
+    else
+        return string.sub(str, 1, 8)
+    end
 end
 
-function cipher:decrypt(d, k, s)
-    return aes:new(k, s, cip(self.size, self.mode), self.hash, self.rounds):decrypt(d)
+function cipher:encrypt(content, key, salt)
+    -- 适配salt超过8位出错
+    salt = salt_substring(salt)
+    local _aes,err = aes:new(key, salt, cip(self.size, self.mode), self.hash, self.rounds)
+    if err then
+        ngx.log(ngx.ERR,"aes.new error: ",err)
+    end
+    return _aes:encrypt(content)
+end
+
+function cipher:decrypt(content, key, salt)
+    -- 适配salt超过8位出错
+    salt = salt_substring(salt)
+    local _aes,err = aes:new(key, salt, cip(self.size, self.mode), self.hash, self.rounds)
+    if err then
+        ngx.log(ngx.ERR,"aes.new error: ",err)
+    end
+    return _aes:decrypt(content)
 end
 
 return cipher
